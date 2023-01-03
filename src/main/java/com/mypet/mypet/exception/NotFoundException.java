46cb7e28c813fdeb50afd39ea3ed5034e7fc8fd9
Elmehdi <93981356+Raikonerr@users.noreply.github.com>
2023-01-03 11:57:24 +0100

    add : exceptions

---
+++ src/main/java/com/mypet/mypet/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package com.mypet.mypet.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
